3302ccd573aa76fc296283400b20fe7b41a340f4
sriselvamtoursandtravels <sivabalaji822060@gmail.com>
2026-09-21 23:39:01 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE='sst-driver-v5.4';
+const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./splash.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
